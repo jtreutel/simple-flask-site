0b4f902e85133b433beb92a519dac604a9df609b
jtreutel <jtreutel+github@protonmail.com>
2020-10-25 19:27:22 -0500

    Add Chrome options to avoid crash.

--- tests/test_sitejs.py
+++ tests/test_sitejs.py
@@ -1,5 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-dev-shm-usage')
 
 def test_webpage_is_up(client):
     res = client.get('/')
@@ -7,7 +13,7 @@ def test_webpage_is_up(client):
 
 def test_text_display(app, client):
     #Click the button and get the resulting text
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(chrome_options=chrome_options)
     driver.get("http://127.0.0.1:5000")
     driver.execute_script("return document.getElementsByTagName('input')[0].click()")
     returned_text = driver.execute_script("return document.body.innerText")
