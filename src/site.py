527a6a53cbb089feef2374796fb226bc1f56175c
jennings <jennings@circleci.com>
2021-01-25 17:15:36 +0900

    Add simple docstring to pass linting.

--- src/site.py
+++ src/site.py
@@ -1,3 +1,4 @@
+"""Creates a simple website for CI/CD demo purposes"""
 from flask import Flask, render_template
 app = Flask(__name__)
 client = app.test_client()
@@ -5,6 +6,7 @@ client = app.test_client()
 
 @app.route("/")
 def index():
-  return render_template("index.html")
+    """Renders site index"""
+    return render_template("index.html")
 if __name__ == '__main__':
-  app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
